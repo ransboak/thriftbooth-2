621e9ce1e1e4f3b1f3fbaedb3ed87a15afb26915
ransboak <66210638+ransboak@users.noreply.github.com>
2023-08-26 07:09:28 -0700

    update

--- frontend/src/components/Products/ProductDetails.jsx
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -151,11 +151,11 @@ const ProductDetails = ({ data }) => {
                 <h1 className={`${styles.productTitle}`}>{data.name}</h1>
                 <p>{data.description}</p>
                 <div className="flex pt-3">
-                  <h4 className={`${styles.productDiscountPrice}`}>
-                    {data.discountPrice}$
+                   <h4 className={`${styles.productDiscountPrice}`}>
+                  ¢{data.discountPrice}
                   </h4>
                   <h3 className={`${styles.price}`}>
-                    {data.originalPrice ? data.originalPrice + "$" : null}
+                    {data.originalPrice ? "¢" + data.originalPrice : null}
                   </h3>
                 </div>
 
